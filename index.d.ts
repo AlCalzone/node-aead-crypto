2cd6327c7cf2fd4f2777ddd65530a6ecd5940ebd
dominic.griesel <d.griesel@gmx.net>
2017-08-23 15:33:46 +0200

    remove unneccessary semicolons [skip ci]

--- index.d.ts
+++ index.d.ts
@@ -10,8 +10,8 @@ export interface DecryptionResult {
 export namespace ccm {
     export function encrypt(key: Buffer, iv: Buffer, plaintext: Buffer, aad: Buffer, authTagLength: number): EncryptionResult;
     export function decrypt(key: Buffer, iv: Buffer, ciphertext: Buffer, aad: Buffer, authTag: Buffer): DecryptionResult;
-};
+}
 export namespace gcm {
     export function encrypt(key: Buffer, iv: Buffer, plaintext: Buffer, aad: Buffer): EncryptionResult;
     export function decrypt(key: Buffer, iv: Buffer, ciphertext: Buffer, aad: Buffer, authTag: Buffer): DecryptionResult;
-};
+}
