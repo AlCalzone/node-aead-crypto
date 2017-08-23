acfcbad87c97e205aabf0d1444f693bf24890652
dominic.griesel <d.griesel@gmx.net>
2017-08-23 10:55:11 +0200

    fixed definition file

--- index.d.ts
+++ index.d.ts
@@ -1,40 +1,17 @@
-declare interface EncryptionResult {
-    ciphertext: Buffer,
-    auth_tag: Buffer
+/// <reference types="node" />
+export interface EncryptionResult {
+    ciphertext: Buffer;
+    auth_tag: Buffer;
 }
-declare interface DecryptionResult {
-    plaintext: Buffer,
-    auth_ok: boolean
+export interface DecryptionResult {
+    plaintext: Buffer;
+    auth_ok: boolean;
 }
-
-declare namespace AeadCrypto {
-    ccm: {
-        encrypt: (
-            key: Buffer, iv: Buffer, 
-            plaintext: Buffer, 
-            aad: Buffer, 
-            authTagLength: number
-        ) => EncryptionResult;
-        decrypt: (
-            key: Buffer, iv: Buffer, 
-            ciphertext: Buffer, 
-            aad: Buffer, 
-            authTag: Buffer
-        ) => DecryptionResult;
-    };
-    gcm: {
-        encrypt: (
-            key: Buffer, iv: Buffer, 
-            plaintext: Buffer, 
-            aad: Buffer
-        ) => EncryptionResult;
-        decrypt: (
-            key: Buffer, iv: Buffer, 
-            ciphertext: Buffer, 
-            aad: Buffer, 
-            authTag: Buffer
-        ) => DecryptionResult;
-    };
+export namespace ccm {
+    export function encrypt(key: Buffer, iv: Buffer, plaintext: Buffer, aad: Buffer, authTagLength: number): EncryptionResult;
+    export function decrypt(key: Buffer, iv: Buffer, ciphertext: Buffer, aad: Buffer, authTag: Buffer): DecryptionResult;
 };
-
-export = AeadCrypto;
+export namespace gcm {
+    export function encrypt(key: Buffer, iv: Buffer, plaintext: Buffer, aad: Buffer): EncryptionResult;
+    export function decrypt(key: Buffer, iv: Buffer, ciphertext: Buffer, aad: Buffer, authTag: Buffer): DecryptionResult;
+};
