a572d4af41635f0b0d93d40c05f7eaccac5d16b3
dominic.griesel <d.griesel@gmx.net>
2018-11-27 13:04:19 +0100

    Rework build matrix

--- ci/prebuild.js
+++ ci/prebuild.js
@@ -3,53 +3,45 @@ const path = require("path");
 const os = require("os");
 
 const isWindows = /^win/.test(os.platform());
+const isLinux = os.platform() === "linux";
+const isDarwin = os.platform() === "darwin";
 
-const versions = [
-	{ runtime: "node", target: "6.3.0", arch: "x64" },
-	{ runtime: "node", target: "6.3.0", arch: "x64" },
-	{ runtime: "node", target: "6.3.0", arch: "x64" },
-	{ runtime: "node", target: 57, arch: "x64" },
-	{ runtime: "node", target: 57, arch: "x64" },
-	{ runtime: "node", target: 57, arch: "x64" },
-	{ runtime: "electron", target: 48, arch: "x64" },
-	{ runtime: "electron", target: 48, arch: "x64" },
-	{ runtime: "electron", target: 48, arch: "x64" },
-	{ runtime: "electron", target: 57, arch: "x64" },
-	{ runtime: "electron", target: 57, arch: "x64" },
-	{ runtime: "electron", target: 57, arch: "x64" },
-];
-if (isWindows) {
-	versions.push(
-		{ runtime: "node", target: "6.3.0", arch: "x86" },
-		{ runtime: "node", target: "6.3.0", arch: "x86" },
-		{ runtime: "node", target: "6.3.0", arch: "x86" },
-		{ runtime: "node", target: 57, arch: "x86" },
-		{ runtime: "node", target: 57, arch: "x86" },
-		{ runtime: "node", target: 57, arch: "x86" },
-		{ runtime: "electron", target: 48, arch: "x86" },
-		{ runtime: "electron", target: 48, arch: "x86" },
-		{ runtime: "electron", target: 48, arch: "x86" },
-		{ runtime: "electron", target: 57, arch: "x86" },
-		{ runtime: "electron", target: 57, arch: "x86" },
-		{ runtime: "electron", target: 57, arch: "x86" },
-	);
+// Take the newest version for each ABI version
+const getRuntimeVersions = (runtime) => {
+	// source: https://nodejs.org/en/download/releases/
+	return runtime === "node" ? ["6.14.4", "8.13.0"]
+		// source: https://github.com/lgeiger/electron-abi
+		: /* runtime === "electron" */["1.2.8", "1.3.13", "1.4.16", "1.7.10"]
+		;
 }
-if (os.platform() === "linux") {
-	versions.push(
-		{ runtime: "node", target: "6.3.0", arch: "arm" },
-		{ runtime: "node", target: "6.3.0", arch: "arm" },
-		{ runtime: "node", target: "6.3.0", arch: "arm" },
-		{ runtime: "node", target: 57, arch: "arm" },
-		{ runtime: "node", target: 57, arch: "arm" },
-		{ runtime: "node", target: 57, arch: "arm" },
-	);
+
+const getArchs = () => {
+	return isWindows ? ["ia32", "x64"]
+		: isLinux ? ["ia32", "x64", "arm"]
+		: ["x64"] // darwin
+		;
+};
+const getRuntimes = (arch) => arch === "arm" ? ["node"] : ["node", "electron"];
+
+function getAllVersions() {
+	const ret = [];
+	for (const arch of getArchs()) {
+		for (const runtime of getRuntimes(arch)) {
+			for (const target of getRuntimeVersions(runtime)) {
+				ret.push({
+					arch, runtime, target,
+				});
+			}
+		}
+	}
+	return ret;
 }
 
 const token = process.env.PREBUILD_TOKEN;
 
 async function main() {
 	const executable = path.join(__dirname, "..", "node_modules/.bin", "prebuild" + (isWindows ? ".cmd" : ""));
-	for (const version of versions) {
+	for (const version of getAllVersions()) {
 		const { exitCode } = await runCommand(
 			executable,
 			[
@@ -61,7 +53,8 @@ async function main() {
 			{ cwd: path.join(__dirname, "..") }
 		)
 		if (exitCode !== 0) {
-			throw new Error(`prebuild exited with code ${exitCode}`);
+			console.error(`WARN: prebuild failed for ${version.runtime}@${version.target} (${version.arch})!`)
+			// throw new Error(`prebuild exited with code ${exitCode}`);
 		}
 	}
 }
