ceebdaa0ec5cce8a51e1a6ff40321c452962ff93
Dominic Griesel <dominic.griesel@nabucasa.com>
2023-01-26 22:42:59 +0100

    fix: remove all references to FreeBSD

--- lib.js
+++ lib.js
@@ -142,21 +142,6 @@ switch (platform) {
         throw new Error(`Unsupported architecture on macOS: ${arch}`)
     }
     break
-  case 'freebsd':
-    if (arch !== 'x64') {
-      throw new Error(`Unsupported architecture on FreeBSD: ${arch}`)
-    }
-    localFileExisted = existsSync(join(__dirname, 'node-aead-crypto.freebsd-x64.node'))
-    try {
-      if (localFileExisted) {
-        nativeBinding = require('./node-aead-crypto.freebsd-x64.node')
-      } else {
-        nativeBinding = require('node-aead-crypto-freebsd-x64')
-      }
-    } catch (e) {
-      loadError = e
-    }
-    break
   case 'linux':
     switch (arch) {
       case 'x64':
--- npm/freebsd-x64/README.md
+++
@@ -1,3 +0,0 @@
-# `node-aead-crypto-freebsd-x64`
-
-This is the **x86_64-unknown-freebsd** binary for `node-aead-crypto`
